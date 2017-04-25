895e9251a056551a78cf3b8c90ad7f865c849722
Chester Lai (RD-TW) <chester_lai@tw-chesterlai.client.tw.trendnet.org>
2017-04-25 14:01:15 +0800

    change

--- b_rsync.sh
+++ b_rsync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #use rsync for back up
 weekday=$(date +%w)
-if [ $weekday == 2 ]; then 
-   /usr/bin/rsync -azvu --exclude-from '/Volumes/backup/rsync_backup/rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/rsync.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/first_half_week/
+if [ $weekday == 1 ] || [ $weekday == 3 ]; then
+   /usr/bin/rsync -azvu --exclude-from '/Volumes/backup/rsync_backup/rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/rsync.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/1_3/
 else
-   /usr/bin/rsync -azvu --exclude-from '/Volumes/backup/rsync_backup/rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/rsync.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/second_half_week/
+   /usr/bin/rsync -azvu --exclude-from '/Volumes/backup/rsync_backup/rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/rsync.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/2_4_5/
 fi
 #/usr/bin/rsync -azvu --exclude-from 'rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/rsync.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/
