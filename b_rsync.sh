599fea877cfd4b32273b68201b9edb0b209ac1bf
Chester Lai (RD-TW) <chester_lai@tw-chesterlai.client.tw.trendnet.org>
2017-05-09 13:31:20 +0800

    add weekly backup

--- b_rsync.sh
+++ b_rsync.sh
@@ -6,4 +6,9 @@ if [ $weekday == 1 ] || [ $weekday == 3 ]; then
 else
    /usr/bin/rsync -azvu --delete --exclude-from '/Volumes/backup/rsync_backup/rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/rsync.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/2_4_5/
 fi
+
+if [ $weekday == 5 ]; then 
+   /usr/bin/rsync -azvu --delete --exclude-from '/Volumes/backup/rsync_backup/rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/r_week.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/weekly/
+fi
+
 #/usr/bin/rsync -azvu --exclude-from 'rsync-exclude.txt' --log-file=/Volumes/backup/rsync_backup/log/rsync.log.`date +%Y-%m-%d` /Users/chester_lai/ /Volumes/backup/rsync_backup/chester_lai/
